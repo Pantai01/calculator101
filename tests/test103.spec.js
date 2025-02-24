1f2e7219ddeb0bf5b9f5b2fd08baa017e9226fac
Pantai01 <a01276114@tec.mx>
2025-02-24 08:05:06 -0600

    UPDATE: All test modif for workflow and runTest.yml

--- tests/test103.spec.js
+++ tests/test103.spec.js
@@ -6,12 +6,50 @@ describe('test3(0,-1)', function() {
   this.timeout(30000)
   let driver
   let vars
+
+  if (!fs.existsSync('./screenshots')) {
+
+    fs.mkdirSync('./screenshots');
+
+  }
+  
   beforeEach(async function() {
-    driver = await new Builder().forBrowser('chrome').build()
+    const chrome = require('selenium-webdriver/chrome');
+
+        const options = new chrome.Options();
+
+        options.addArguments('--headless', '--no-sandbox', '--disable-dev-shm-usage');
+
+        driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+
     vars = {}
   })
   afterEach(async function() {
-    await driver.quit();
+    if (driver) {
+
+      // Take a screenshot of the result page
+
+      const filename = this.currentTest.fullTitle()
+
+          .replace(/['"]+/g, '')
+
+          .replace(/[^a-z0-9]/gi, '_')
+
+          .toLowerCase();;
+
+      const encodedString = await driver.takeScreenshot();
+
+      await fs.writeFileSync(`./screenshots/${filename}.png`,
+
+          encodedString, 'base64');
+
+
+
+      // Close the browser
+
+      await driver.quit();
+
+    }
   })
   it('test3(0,-1)', async function() {
     await driver.get("http://127.0.0.1:3000/")
@@ -21,5 +59,14 @@ describe('test3(0,-1)', function() {
     await driver.findElement(By.id("num2")).click()
     await driver.findElement(By.id("num2")).sendKeys("-1")
     await driver.findElement(By.css("button:nth-child(1)")).click()
+
+    const filename = 'test103';
+
+            const encodedString = await driver.takeScreenshot();
+
+            await fs.writeFileSync(`./screenshots/${filename}.png`,
+
+                encodedString, 'base64');
+
   })
 })
