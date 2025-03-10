205bf3a49d828f62cc0f63848f5c810daf7b6473
Pantai01 <a01276114@tec.mx>
2025-03-10 08:30:09 -0600

    Feat: Implementation of frontend fetching results from backend server.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,22 @@
+//Running server
+//py -m http.server 8000
+
 
 function addition(){
     const num1 = document.getElementById('num1').value;
     const num2 = document.getElementById('num2').value; 
 
 
-    ///When not using Backend Server
+    /*///When not using Backend Server
     let result;
     result = Number(num1) + Number(num2);
     document.getElementById('operator').innerText = "+";
     document.getElementById('result').innerText = "Result: " + result;
-    console.log("Addition of " + num1 + " and " + num2 + " is " + result);
+    console.log("Addition of " + num1 + " and " + num2 + " is " + result);*/
 
 
     //Using the Backedend Server
-    /*fetch(`http://localhost:8080/add?num1=${num1}&num2=${num2}`)
+    fetch(`http://localhost:8080/add?num1=${num1}&num2=${num2}`)
         .then(response => response.json())
         .then(data => {
             document.getElementById('operator').innerText = "+";
@@ -21,7 +24,6 @@ function addition(){
             console.log("Addition of " + num1 + " and " + num2 + " is " + data.result);
         })
         .catch(error => console.error('Error:', error));
-    */
 }
 
 function substraction(){
@@ -29,16 +31,16 @@ function substraction(){
     const num2 = document.getElementById('num2').value;
     document.getElementById('operator').innerText = "-";
     
-    ///When not using Backend Server
+    /*///When not using Backend Server
     let result;
     result = Number(num1) - Number(num2);
 
     document.getElementById('result').innerText = "Result: " + result;
-    console.log("Substraction of " + num1 + " and " + num2 + " is " + result);
+    console.log("Substraction of " + num1 + " and " + num2 + " is " + result);*/
 
 
     //Using the Backend Server
-    /*fetch(`http://localhost:8080/sub?num1=${num1}&num2=${num2}`)
+    fetch(`http://localhost:8080/sub?num1=${num1}&num2=${num2}`)
         .then(response => response.json())
         .then(data => {
             document.getElementById('operator').innerText = "-";
@@ -46,7 +48,6 @@ function substraction(){
             console.log("Substraction of " + num1 + " - " + num2 + " is " + data.result);
         })
         .catch(error => console.error('Error:', error));
-    */
 }
 
 function resetFields(){
